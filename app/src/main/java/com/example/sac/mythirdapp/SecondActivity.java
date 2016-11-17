fb0c0fbd46d8a8c3581bee39902d46876328105e
Sachin Naik <srnaik@hotmail.com>
2016-11-16 22:49:12 -0800

    Hotfix changes

---
+++ app/src/main/java/com/example/sac/mythirdapp/SecondActivity.java
@@ -0,0 +1,17 @@
+package com.example.sac.mythirdapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Naik Junior on 11/16/2016.
+ */
+
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
